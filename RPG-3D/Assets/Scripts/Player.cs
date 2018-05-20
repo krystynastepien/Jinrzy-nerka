02a32c16d259fe19839802740b7f360c29c07904
krystynastepien <krystyna.stepien91@gmail.com>
2018-05-20 20:56:14 +0200

    poruszanie się za kliknięciem myszy
    problem: po pewnym czasie się kraszuje? niby linia 49 w skrypcie Player.cs ale ja nie wiem czemu on tego nie robi, może coś w prefabs jest źle?
    
    tutorial:   https://www.youtube.com/watch?v=R7CUzpERCIY

---
+++ RPG-3D/Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Variables
+    public float movementSpeed = 1;
+    private float hitDistance = 0.0f;  //y, czyli zawsze bd na ziemi bo tam jest teren
+    private bool moving;
+    public GameObject PlayerMovePoint;
+    private Transform PMP;
+    private bool pmpSpawned;
+    private GameObject triggeringPMP;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //player movement
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        //Terrain playerTerrain = new Terrain();
+
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (playerPlane.Raycast(ray, out hitDistance))  //jeśli playerplane jest uderzony przez ray na ekranie gry
+        {
+            Vector3 mouseClickPosition = ray.GetPoint(hitDistance);
+            Quaternion targetRotation = Quaternion.LookRotation(mouseClickPosition - transform.position);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                moving = true;
+
+                if (pmpSpawned)
+                {
+                    PMP = null;
+                    PMP = Instantiate(PlayerMovePoint.transform, mouseClickPosition, Quaternion.identity);
+                }
+                else
+                {
+                    PMP = Instantiate(PlayerMovePoint.transform, mouseClickPosition, Quaternion.identity);
+                }
+                // print(mouseClickPosition);
+            }
+
+            if (PMP)
+            {
+                pmpSpawned = true;
+            }
+            else
+            {
+                pmpSpawned = false;
+            }
+
+            if (moving)
+            {
+                Move();
+            } 
+        }
+    }
+
+    public void Move()
+    {
+        if (PMP)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PMP.transform.position, movementSpeed);
+            this.transform.LookAt(PMP.transform);
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PMP")
+        {
+            triggeringPMP = other.gameObject;
+            triggeringPMP.GetComponent<PMP>().DestroyPMP();
+        }
+    }
+
+}
