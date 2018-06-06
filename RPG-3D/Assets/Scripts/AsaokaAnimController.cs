7e5f8a7795816c7ec754c949622f673b1d94cb94
krystynastepien <krystyna.stepien91@gmail.com>
2018-06-07 00:34:40 +0200

    walking + idle

---
+++ RPG-3D/Assets/Scripts/AsaokaAnimController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsaokaAnimController : MonoBehaviour {
+
+    public GameObject chara;
+
+    static Animator anim;
+
+    private PlayerScript playerScr;
+
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        playerScr = chara.GetComponent<PlayerScript>();
+     
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        if(playerScr.moving == true) // jesli sie ruszamy po horizontal albo vertical
+        {
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
+    }
+}
---
+++ RPG-3D/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    // Variables
+    public float movementSpeed = 1;
+    private float hitDistance = 0.0f;  //y, czyli zawsze bd na ziemi bo tam jest teren
+    public bool moving;
+    public GameObject PlayerMovePoint;
+    private Transform PMP;
+    private bool pmpSpawned;
+    private GameObject triggeringPMP;
+
+ 
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+   
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+       
+        //player movement
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        //Terrain playerTerrain = new Terrain();
+
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (playerPlane.Raycast(ray, out hitDistance))  //jeśli playerplane jest uderzony przez ray na ekranie gry
+        {
+            Vector3 mouseClickPosition = ray.GetPoint(hitDistance);
+            Quaternion targetRotation = Quaternion.LookRotation(mouseClickPosition - transform.position);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                moving = true;
+            
+                if (!pmpSpawned)
+                {
+
+                    PMP = Instantiate(PlayerMovePoint.transform, mouseClickPosition, Quaternion.identity);
+                    //PMP = null;
+                    
+                   // PMP = Instantiate(PlayerMovePoint.transform, mouseClickPosition, Quaternion.identity);
+                }
+                else
+                {
+                    //PMP = Instantiate(PlayerMovePoint.transform, mouseClickPosition, Quaternion.identity);
+                    PMP.transform.position = mouseClickPosition;
+                }
+                // print(mouseClickPosition);
+            }
+
+            if (PMP)
+            {
+                pmpSpawned = true;
+            }
+            else
+            {
+                pmpSpawned = false;
+            }
+
+            if (moving)
+            {
+                Move();
+            } 
+        }
+    }
+
+    public void Move()
+    {
+        if (PMP)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PMP.transform.position, movementSpeed);
+            this.transform.LookAt(PMP.transform);
+            if (transform.position == PMP.transform.position)
+            {
+                moving = false;
+            }
+          
+        }
+    }
+
+/*
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PMP")
+        {
+            triggeringPMP = other.gameObject;
+            triggeringPMP.GetComponent<PMP>().DestroyPMP();
+        }
+    }
+*/
+}
